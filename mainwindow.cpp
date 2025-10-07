7d8888ef5364736d6335a0fb364d764be7c6e77c
salsabil745 <Salsabil.khoualdi@esprit.tn>
2025-10-07 14:47:45 +0100

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,16 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include "abonne.h"
-#include"dialog.h"
+#include <QDebug>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this); 
+    ui->setupUi(this);
+    ui->image->setPixmap(QPixmap("C:/Users/hp/Documents/abonne/images/logo.png"));
+    ui->image->setScaledContents(true);
+
 }
 
 MainWindow::~MainWindow()
@@ -15,20 +18,5 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
-{
-    abonne a;
-    a.setID(ui->lineEdit_ID->text());
-    a.setNom(ui->lineEdit_Nom->text());
-    a.setPrenom(ui->lineEdit_Prenom->text());
-    a.setdatedenaissance(ui->lineEdit_datedenaissance->text());
-    a.setAdresse(ui->lineEdit_Adresse->text());
-    a.setNumerodetelephone(ui->lineEdit_Numerodetelephone->text());
-    a.setEmail(ui->lineEdit_Email->text());
 
-    Dialog d;
-    d.setabonne(a);
-    d.exec();
-
-}
 
